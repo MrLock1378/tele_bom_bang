dd2361e0b55035347b0b372fc4414e4507a76530
MrLock1378 <rezafaghihi1379@gmail.com>
2017-04-14 11:46:51 +0430

    Update Write.lua

--- plugins/Write.lua
+++ plugins/Write.lua
@@ -8,7 +8,7 @@ end
 
 local function run(msg, matches)
 	if #matches < 2 then
-		return "بعد از این دستور، با قید یک فاصله کلمه یا جمله ی مورد نظر را جهت زیبا نویسی وارد کنید"
+		return "بعد از این `دستور`، با قید `یک فاصله` کلمه یا جمله ی مورد نظر را جهت `زیبا نویسی `وارد کنید"
 	end
 	if string.len(matches[2]) > 20 then
 		 tdcli.sendMessage(msg.chat_id_, 0, 1, "حداکثر حروف مجاز 20 کاراکتر انگلیسی و عدد است", 1, 'html')
